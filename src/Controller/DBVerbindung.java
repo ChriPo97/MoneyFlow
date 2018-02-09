ceac24e044544acc5078663d4ff986552186d877
Josua <Josua@10.10.82.228>
2018-02-09 12:42:13 +0100

    ArtikelDBAbfragen MWSTAbfrage

--- src/Controller/DBVerbindung.java
+++ src/Controller/DBVerbindung.java
@@ -17,13 +17,21 @@ public class DBVerbindung {
 
     private static Connection con = null;
     private static String url = "jdbc:sqlite:C:/Programme/SQLiteStudio/MoneyDB.db";
-    private static PreparedStatement ps = null;
-    private static ResultSet rs = null;
+    private static PreparedStatement ps;
+    private static ResultSet rs;
 
     public static void main(String args[]) {
 
-        Statement stmt = null;
-        ResultSet result = null;
+        verbinden();
+        // System.out.println(artikelNametoArtikelID("Apfel"));
+        // System.out.println(artikelNametoKategorie("Stuhl"));
+        //System.out.println(artikelNametoArtikelNummer("Bier"));
+        System.out.println(artikelNametoMehrwersteuerklasse("Apfel"));
+        //System.out.println(artikelNametoEinheit("Bier"));
+        //System.out.println(artikelNametoPreis("Bier"));
+        System.out.println(mehrwersteuerklassetoMehrwertsteuer("1"));
+        
+        verbindungSchliessen();
 
         /*         stmt = con.createStatement();
             String sql = "SELECT * FROM ARTIKEL";
@@ -82,5 +90,101 @@ public class DBVerbindung {
             Logger.getLogger(DBVerbindung.class.getName()).log(Level.SEVERE, null, ex);
         }
         return artikelID;
+    }
+
+    //zu gegebenem Artikelnamen Kategorie ausgeben
+    public static String artikelNametoKategorie(String artikelname) {
+        String kategorie = "";
+        try {
+            ps = con.prepareStatement("SELECT Kategorie FROM Artikel WHERE Artikelname = ?");
+            ps.setString(1, artikelname);
+            rs = ps.executeQuery();
+            rs.next();
+            kategorie = rs.getString("Kategorie");
+            rs.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DBVerbindung.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return kategorie;
+    }
+
+    //zu gegebenem Artikelnamen Artikelnummer ausgeben
+    public static int artikelNametoArtikelNummer(String artikelname) {
+        int artikelnummer = 0;
+        try {
+            ps = con.prepareStatement("SELECT Artikelnummer FROM Artikel WHERE Artikelname = ?");
+            ps.setString(1, artikelname);
+            rs = ps.executeQuery();
+            rs.next();
+            artikelnummer = rs.getInt("Artikelnummer");
+            rs.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DBVerbindung.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return artikelnummer;
+    }
+
+    //zu gegebenem Artikelnamen Preis ausgeben
+    public static int artikelNametoPreis(String artikelname) {
+        int preis = 0;
+        try {
+            ps = con.prepareStatement("SELECT Preis FROM Artikel WHERE Artikelname = ?");
+            ps.setString(1, artikelname);
+            rs = ps.executeQuery();
+            rs.next();
+            preis = rs.getInt("Preis");
+            rs.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DBVerbindung.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return preis;
+    }
+
+    //zu gegebenem Artikelnamen Einheit ausgeben
+    public static String artikelNametoEinheit(String artikelname) {
+        String einheit = "";
+        try {
+            ps = con.prepareStatement("SELECT Einheit FROM Artikel WHERE Artikelname = ?");
+            ps.setString(1, artikelname);
+            rs = ps.executeQuery();
+            rs.next();
+            einheit = rs.getString("Einheit");
+            rs.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DBVerbindung.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return einheit;
+    }
+
+    //zu gegebenem Artikelnamen Mehrwertsteuerklasse ausgeben
+    public static String artikelNametoMehrwersteuerklasse(String artikelname) {
+        String mwstk = "";
+        try {
+            ps = con.prepareStatement("SELECT Mehrwertsteuerklasse FROM Artikel WHERE Artikelname = ?");
+            ps.setString(1, artikelname);
+            rs = ps.executeQuery();
+            rs.next();
+            mwstk = rs.getString("Mehrwertsteuerklasse");
+            rs.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DBVerbindung.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return mwstk;
+    }
+
+    //Mehrwertsteuer bei gegebener Mehrwertsteuerklasse ausgeben
+    public static float mehrwersteuerklassetoMehrwertsteuer(String mehrwertsteuerklasse) {
+        float mwst = 0;
+        try {
+            ps = con.prepareStatement("SELECT  Steuersatz FROM Mehrwertsteuer WHERE MID = ?");
+            ps.setString(1, mehrwertsteuerklasse);
+            rs = ps.executeQuery();
+            rs.next();
+            mwst = rs.getFloat("Steuersatz");
+            rs.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(DBVerbindung.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return mwst;
     }
 }
